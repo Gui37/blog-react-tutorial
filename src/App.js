f2faca720b793a306eca2513af454cc8829d8ba3
Gui <guifty32@gmail.com>
2025-02-09 22:08:03 +0200

    Dynamic value on template

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import './App.css';
 
 function App() {
+  const title = 'Bem-vindo ao blog'
+  const views = 50;
+  const site = "https://www.bing.com";
   return (
     <div className="App">
       <div className="content">
-        <h1>App Component</h1>
+        <h1>{title}</h1>
+        <p>Visto mais de {views} vezes</p>
+        <a href={site}> Bing website</a>
       </div>
     </div>
   );
