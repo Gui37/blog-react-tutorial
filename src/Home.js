88b4120588bd7302dd19d0f9bdc9a3bee3845b9f
Gui <guifty32@gmail.com>
2025-03-09 19:29:19 +0200

    Reusing Components

--- src/Home.js
+++ src/Home.js
@@ -4,12 +4,14 @@ const Home = () =>{
     const [blogs, setBlogs] = useState([
         {title:'First One',body:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum itaque consectetur accusamus minima',author:'Dante',id:1},
         {title:'Second One',body:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum itaque consectetur accusamus minima',author:'Sparda',id:2},
-        {title:'Third One',body:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum itaque consectetur accusamus minima',author:'Nero',id:3},
+        {title:'Third One',body:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum itaque consectetur accusamus minima',author:'Dante',id:3},
         {title:'Last One',body:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum itaque consectetur accusamus minima',author:'Vergil',id:4}
     ]);
     return(
         <div className="home">
             <BlogList blogs={blogs} title ='All the blogs'/>            
+            <BlogList blogs={blogs.filter((blog) => blog.author === 'Dante')} title ="Dante's blogs"/>            
+
         </div>
     );
 }
