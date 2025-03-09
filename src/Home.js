b2c8befcceb20ca672470610b8e3867811c39ebc
Gui <guifty32@gmail.com>
2025-03-09 18:02:48 +0200

    Click events

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,15 @@
 const Home = () =>{
+    const handleClick = () => {
+        console.log("Hello There");
+    }
+    const handleClickAgain = (name) =>{
+        console.log("Hello There! "+ name)
+    }
     return(
         <div className="home">
             <h1>Página Inicial</h1>
+            <button onClick={handleClick}>Clique aqui!</button>
+            <button onClick={() =>handleClickAgain("Antonio")}>Clique Aqui Denovo</button>
         </div>
     );
 }
