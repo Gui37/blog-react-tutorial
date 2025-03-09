ac42dcf1c7614765694d920c311176aea62343e5
Gui <guifty32@gmail.com>
2025-03-09 20:52:12 +0200

    Conditional Loading Message

--- src/Home.js
+++ src/Home.js
@@ -2,6 +2,7 @@ import { useState, useEffect } from "react";
 import BlogList from "./BlogList";
 const Home = () =>{
     const [blogs, setBlogs] = useState(null);
+    const [isLoading, setIsLoading] = useState(true);
 
     useEffect(()=>{
     fetch('http://localhost:8000/blogs')
@@ -9,11 +10,13 @@ const Home = () =>{
         return res.json()
     })
     .then(data =>{
-        setBlogs(data)
+        setBlogs(data);
+        setIsLoading(false);
     })
     },[]);
     return(
         <div className="home">
+            {isLoading && <div>Carregando os Dados . . .</div>}
             {blogs && <BlogList blogs={blogs} title ='All the blogs' />}
         </div>
     );
