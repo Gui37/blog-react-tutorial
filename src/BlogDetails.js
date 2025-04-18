69151e5952d96e9dcbefa1973faa9991c0d2ffd7
Gui <guifty32@gmail.com>
2025-04-18 22:19:07 +0200

    delete method added

--- src/BlogDetails.js
+++ src/BlogDetails.js
@@ -1,9 +1,18 @@
 import { useParams } from "react-router-dom";
 import useFetch from "./useFetch";
+import { useHistory } from "react-router-dom/cjs/react-router-dom.min";
 
 const BlogDetails = () =>{
     const {id}  = useParams();
     const {data:blog, error, isLoading} = useFetch('http://localhost:8000/blogs/' +id);
+    const history = useHistory();
+    const handleApagar = ()=>[
+        fetch('http://localhost:8000/blogs/'+ blog.id,{
+            method:"DELETE"
+        }).then(() =>{
+            history.pushState("/");
+        })
+    ]
     return(
 <div className="blog-details">
 {isLoading && <div>Carregando os Dados . . .</div>}
@@ -13,6 +22,7 @@ const BlogDetails = () =>{
         <h2>{blog.title}</h2>
         <p>Escrito Por: {blog.author}</p>
         <div>{blog.body}</div>
+        <button onClick={handleApagar}>Apagar Blog</button>
     </article>
 )}
 </div>
